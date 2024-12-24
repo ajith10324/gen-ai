380a7d744874c61855bb5eee00a63129ef0ebf97
Ajith N <ajithn10324@gmail.com>
2024-12-24 08:31:10 +0530

    Add files via upload
    Gemini Ai Clone

---
+++ app.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import streamlit as st
+import os
+from PIL import Image
+import google.generativeai as genai
+
+os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+def get_response(input, image):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    if input != '':
+        response = model.generate_content([input,image])
+    else:
+        response = model.generate_content(image)
+    return response.text
+
+
+
+st.set_page_config(page_title = 'Gemini image Demo')
+st.header("Gemini Ai")
+input = st.text_input('Input Prompt :',key = 'input')
+
+uploaded_image =st.file_uploader('Choose an image...',type = ['jpg','jpeg','png'])
+image = ''
+if uploaded_image is not None:
+    image = Image.open(uploaded_image)
+    st.image(image,caption='Uploaded Image',use_column_width=True)
+submit = st.button('Tell Me About This Image')
+if submit:
+    response = get_response(input,image)
+    st.subheader('The response is ')
+    st.write(response)
